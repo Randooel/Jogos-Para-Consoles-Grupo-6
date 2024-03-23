2da1570e3988682fbb06cae3e70113993f99a7aa
Ervilha <ervilha.developer@gmail.com>
2024-03-23 18:55:43 -0300

    Merges changelog v0.02 - mudança do texto
    Mudança do texto "Buenos dias, Mundo" para "Hello World"

--- JC-Atividade 2.1/JC-Atividade 2.1/Program.cs
+++ JC-Atividade 2.1/JC-Atividade 2.1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 private void HelloWorld()
 {
-    Console.WriteLine("Buenos dias, Mundo!");
+    Console.WriteLine("Hello World");
 }
