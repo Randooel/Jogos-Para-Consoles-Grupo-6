ef3065938d17f51f890801865a56df54b963685b
Ervilha <ervilha.developer@gmail.com>
2024-03-23 18:40:33 -0300

    ConflictBranch changelog v0.01 - Alteração do texto
    Mudanças:
     - Linha 4 do arquivo Program.cs
       - Foi feita uma alteração no texto "Hello World", para o texto "Buenos dias, Mundo".

--- JC-Atividade 2.1/JC-Atividade 2.1/Program.cs
+++ JC-Atividade 2.1/JC-Atividade 2.1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 private void HelloWorld()
 {
-    Console.WriteLine("Hello, World!");
+    Console.WriteLine("Buenos dias, Mundo!");
 }
