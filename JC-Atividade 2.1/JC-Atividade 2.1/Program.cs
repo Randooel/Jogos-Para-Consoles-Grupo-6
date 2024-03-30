6c53606f2e25d7c9001c742661282522f46c1502
Joao-M-Bastos <jmb32180@gmail.com>
2024-03-30 15:23:43 -0300

    remake

--- JC-Atividade 2.1/JC-Atividade 2.1/Program.cs
+++ JC-Atividade 2.1/JC-Atividade 2.1/Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-private void HelloWorld()
+private void main()
+{
+    Print()
+}
+
+public void Print()
 {
     Console.WriteLine("Hello, carinha!");
 }
