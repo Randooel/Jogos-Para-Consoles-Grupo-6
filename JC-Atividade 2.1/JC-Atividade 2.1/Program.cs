0cee61af216daad924c5470b46cfef7812e9ca8d
Randooel <114538289+Randooel@users.noreply.github.com>
2024-03-30 15:00:55 -0300

    Revert "ConflictBranch changelog v0.02 - tentativa de erro"
    This reverts commit 0cb029385f6e4081d013ac1fde127b6cdb567905.

--- JC-Atividade 2.1/JC-Atividade 2.1/Program.cs
+++ JC-Atividade 2.1/JC-Atividade 2.1/Program.cs
@@ -1,10 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 private void HelloWorld()
 {
-<<<<<<< Updated upstream
     Console.WriteLine("Buenos dias, Mundo!");
-=======
-    Console.WriteLine("Hello World");
-    Console.WriteLine("Fala ai Carinaldo");
->>>>>>> Stashed changes
 }
