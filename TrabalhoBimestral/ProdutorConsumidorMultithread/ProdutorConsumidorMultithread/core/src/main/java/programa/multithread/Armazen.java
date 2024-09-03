df604da167f3823631a1aa58479c8b46edf7c25a
Randooel <leonardo.alves.barros.1@gmail.com>
2024-09-02 21:21:50 -0300

    WIP Main

--- TrabalhoBimestral/ProdutorConsumidorMultithread/ProdutorConsumidorMultithread/core/src/main/java/programa/multithread/Armazen.java
+++ TrabalhoBimestral/ProdutorConsumidorMultithread/ProdutorConsumidorMultithread/core/src/main/java/programa/multithread/Armazen.java
@@ -1,14 +1,28 @@
 package programa.multithread;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Armazen {
 
-	double capacidadeMaxima;
-	
-	public void adicionarRecurso() {
-		
-	}
-	
-	public void removerRecurso() {
-		
-	}
+    private List<String> itens;
+    private double capacidadeMaxima;
+
+    public Armazen(double capacidadeMaxima) {
+        this.itens = new ArrayList<>();
+        this.capacidadeMaxima = capacidadeMaxima;
+    }
+
+    public void adicionarRecurso(String novoItem) {
+        if (this.itens.size() < capacidadeMaxima) {
+            this.itens.add(novoItem);
+            System.out.println("Recurso " + novoItem + " foi adicionado ao armazém.");
+        } else {
+            System.out.println("Armazém cheio! Não é possível adicionar mais recursos.");
+        }
+    }
+
+    public List<String> getItens() {
+        return this.itens;
+    }
 }
--- TrabalhoBimestral/ProdutorConsumidorMultithread/ProdutorConsumidorMultithread/core/src/main/java/programa/multithread/Main.java
+++ TrabalhoBimestral/ProdutorConsumidorMultithread/ProdutorConsumidorMultithread/core/src/main/java/programa/multithread/Main.java
@@ -1,39 +1,69 @@
 package programa.multithread;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private BitmapFont font;
     
-    public static void main(String[] args)
-    {
-    	Produtor objetoProd = new Produtor();
-    }
+    private Produtor produtor;  // Variável para armazenar a instância do Produtor
+    private Armazen armazem;    // Armazém para guardar os recursos
 
+    private String ultimoRecursoColetado;
+    
     @Override
     public void create() {
+        // Inicializando os objetos gráficos
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+        font = new BitmapFont(); // Fonte para mostrar o texto na tela
+
+        // Inicializando o Produtor e Armazém
+        produtor = new Produtor(2.0);  // Produtor com velocidade de produção de 2 segundos
+        armazem = new Armazen(5);  // Armazém com capacidade máxima de 5 itens
+
+        // Inicializando o último recurso coletado
+        ultimoRecursoColetado = "Nenhum recurso coletado";
     }
 
     @Override
     public void render() {
+        // Limpa a tela
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
+        // Coleta um recurso novo a cada frame
+        Recurso.RecursoBase recursoColetado = produtor.coletarRecurso();
+        armazem.adicionarRecurso(recursoColetado.getTipo());
+        ultimoRecursoColetado = recursoColetado.getTipo();
+
+        // Inicia o desenho
         batch.begin();
         batch.draw(image, 140, 210);
+
+        // Exibe o último recurso coletado
+        font.draw(batch, "Último recurso coletado: " + ultimoRecursoColetado, 20, 450);
+
+        // Exibe os itens no armazém
+        font.draw(batch, "Itens no armazém:", 20, 420);
+        int y = 400;
+        for (String item : armazem.getItens()) {
+            font.draw(batch, "- " + item, 20, y);
+            y -= 20;  // Reduz a posição vertical para mostrar o próximo item
+        }
+
         batch.end();
     }
 
     @Override
     public void dispose() {
+        // Libera os recursos
         batch.dispose();
         image.dispose();
+        font.dispose();
     }
 }
