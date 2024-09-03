5e7cd02dc2ae2a10cb2d74945bd89bf7bbd58f59
Ervilha <ervilha.developer@gmail.com>
2024-09-02 23:03:27 -0300

    Revert "Consumidor WIP"
    This reverts commit fab03d83cadd51672e87fc7b27c585b1bcca3385.

--- TrabalhoBimestral/ProdutorConsumidorMultithread/ProdutorConsumidorMultithread/core/src/main/java/programa/multithread/Consumidor.java
+++ TrabalhoBimestral/ProdutorConsumidorMultithread/ProdutorConsumidorMultithread/core/src/main/java/programa/multithread/Consumidor.java
@@ -1,59 +1,8 @@
 package programa.multithread;
 
-import java.util.Random;
-
 public class Consumidor {
 
-	private String nome;
-	private double velocidadeConsumo;
-	private Random random;
-	
-	public Consumidor(String nome, double velocidadeConsumo) {
-        this.nome = nome;
-        this.velocidadeConsumo = velocidadeConsumo;
-        this.random = new Random();
-    }
-	
-	// Método para consumir um recurso do armazém
-    public void consumirRecurso(Armazen armazem) {
-        new Thread(() -> {
-            while (true) {
-                try {
-                    // Tempo necessário para consumir um recurso com base na velocidade de consumo
-                    Thread.sleep((long) (velocidadeConsumo * 1000));
-
-                    if (!armazem.getItens().isEmpty()) {
-                        // Remove o recurso mais antigo ou pode implementar uma lógica de prioridade
-                        String recurso = armazem.getItens().remove(0);
-                        System.out.println(nome + " consumiu o recurso: " + recurso);
-
-                        //Lógica para usar o recurso consumido, tipo construir um item
-                        utilizarRecurso(recurso);
-                    } else {
-                        System.out.println("Armazém vazio! " + nome + " está esperando por novos recursos.");
-                    }
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    break;
-                }
-            }
-        }).start();
-    }
-    
- // Método para utilizar o recurso consumido
-    public void utilizarRecurso(String recurso) {
-        
-        System.out.println(nome + " está utilizando o recurso: " + recurso);
-    }
-    
- // Método para definir a velocidade de consumo
-    public void setVelocidadeConsumo(double velocidade) {
-        this.velocidadeConsumo = velocidade;
-    }
-    
- // Método para obter a velocidade de consumo
-    public double getVelocidadeConsumo() {
-        return velocidadeConsumo;
-    }
-
+	public void utilizarRecurso() {
+		
+	}
 }
